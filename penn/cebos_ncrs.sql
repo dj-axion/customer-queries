ebb04ffa9c72d360466f2789bb46cc2cbf41fa98
DJ Thompson <dj.thompson@axionray.com>
2023-05-25 19:33:06 -0700

    Update with root cause, machine, process

--- penn/cebos_ncrs.sql
+++ penn/cebos_ncrs.sql
@@ -18,13 +18,30 @@ SELECT
 	IIF(ncr.EscalatetoSCAR_f = 1, 'TRUE', 'FALSE') as "Escalate to SCAR",
     initby.DisplayExpression_f as "Initiated By",
     champ.DisplayExpression_f as Champion,
-    psrc.DisplayExpression_f as Category,--Category
+    ncat.Category_f as Category,--Category
     item.DisplayExpression_f as "Failed Item",--Failed Item
     cust.DisplayExpression_f as Customer,--Customer
-    site.SiteName_f as Site,--Site
-    rsite.SiteName_f as "Responsible Site",--Responsible Site
+    site.DisplayExpression_f as Site,--Site
+    rsite.DisplayExpression_f as "Responsible Site",--Responsible Site
     psrc.DisplayExpression_f as "Problem Source",--Problem Source
-    supp.DisplayExpression_f as Supplier--Supplier
+    supp.DisplayExpression_f as Supplier,--Supplier
+	pdis.DisplayExpression_f as "Problem Disposition",--Problem Diposition
+	ncr.DispositionNotes_f as "Disposition Notes",--Disposition Notes
+	psev.DisplayExpression_f as "Problem Severity",--Problem Severity
+	psev.SeverityCriteria_f as "Severity Criteria",--Problem Severity Critieria met
+	ncr.ItemCost_f * ncr.Quantity_f as "Derived Cost",
+	ncr.InitialMaterialCost_f as "Initial Material Cost",--cost
+	xp.MachineType_xf as "Machine Type",--machine
+	prcs.DisplayExpression_f as Process,--process
+	--ncr.EscapeRootCause_f as "Escape Root Cause",--escaperootcause - code
+	ncr.EscapeCauseNotes_f as "Escape Cause Notes",--escapecausenotes
+	ncr.EscapeCAPlan_f as "Escape CA Plan",--escapecorrectiveaction
+	--ncr.OccurrenceRootCause_f as "Occurrence Root Cause",--occurrencerootcause - code
+	ncr.OccurrenceCauseNotes_f as "Occurrence Cause Notes",--occurrencecausenotes
+	ncr.OccurrenceCAPlan_f as "Occurrence CA Plan", --occurrencecorrectiveaction
+	--ncr.SystemicRootCause_f as "Systemic Root Cause",--Systemicrootcause - code
+	ncr.SystemicCauseNotes_f as "Systemic Cause Notes",--Systemiccausenotes
+	ncr.SystemicCAPlan_f as "Systemic CA Plan" --Systemiccorrectiveaction
 FROM
 	Nonconformances_p as ncr
 LEFT JOIN __State as st
@@ -41,14 +58,27 @@ LEFT JOIN Sites_p as rsite
 	ON ncr.ResponsibleSite_f = rsite.Id
 LEFT JOIN Companies_p as supp
 	ON ncr.Supplier_f = supp.Id
+LEFT JOIN NonconformancesCategory_p as ncat
+	ON ncr.Category_f = ncat.Id
 LEFT JOIN ProblemCategory_p as psrc
 	ON ncr.ProblemCategory_f = psrc.Id
 LEFT JOIN Items_p as item
 	ON ncr.FailedItem_f = item.Id
 LEFT JOIN Companies_p as cust
 	ON ncr.Customer_f = cust.Id
+LEFT JOIN ProblemDispositions_p as pdis
+	ON ncr.ProblemDisposition_f = pdis.Id
+LEFT JOIN ProblemSeverities_p as psev
+	ON ncr.ProblemSeverity_f = psev.Id
+LEFT JOIN Processes_p as prcs
+	ON ncr.Process_f = prcs.Id
+LEFT JOIN Nonconformances_xp as xp
+	ON ncr.Id = xp.Id
 WHERE
-	ncr.InitiatedDate_f >= '2020-01-01'
-	AND ncr.Site_f IN (408, 405, 403, 402, 401, 400, 398, 396, 395, 
-						393, 390, 389, 388, 386, 384, 382, 381, 380, 
-						379, 378, 377, 371, 370, 369, 367, 364, 362)
+	ncr.InitiatedDate_f >= '2022-12-01' --when cleared w/ Paula, change to L30
+	AND ncr.Site_f IN (408, 405, 403, 402, 401, 398, 395, 
+						393, 390, 389, 388, 386, 384, 382, 381, 
+						379, 378, 377, 371, 367, 364, 362)
+	AND ncr.NonconformanceNumber_f > 30322
+	--AND ncr.NonconformanceNumber_f <= 30322
+ORDER BY ncr.NonconformanceNumber_f DESC
